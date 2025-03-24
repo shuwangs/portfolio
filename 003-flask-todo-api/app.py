0696d0a9dd05f7fb290a16402d89496d640d2867
Shu <shuw425@gmail.com>
2025-03-24 02:05:49 -0400

    update

--- 003-flask-todo-api/app.py
+++ 003-flask-todo-api/app.py
@@ -1,58 +1,78 @@
 from flask import Flask, request, jsonify
+import sqlite3
+import os
 
 app = Flask(__name__)
+DB_FILE = 'todo.db'
 
-# Use a global list to mimic the database
-todos = []
-# add id to the list
-next_id = 1
+# Check if the database file exists, if not create it
+def init_db():
+    with sqlite3.connect(DB_FILE) as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS tasks (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                task TEXT NOT NULL
+            )
+        ''')
+        conn.commit()
 
 
 @app.route('/')
 
 def home():
-    return "Hello, Flask! Welcome to my app."
+    return "Flask Todo API with SQLite is running!."
 
 @app.route("/add", methods=['POST'])
 def add_task():
-
-    global next_id
     data = request.get_json()
     task_content = data.get('task')
 
     if not task_content:
         return jsonify({"Error: Tasck content missing.\n"}), 400
 
-    # Assign the task with a unique ID
-    task = {"id":next_id, "task": task_content}
-    todos.append(task)
-    next_id += 1  # Increment the ID for the next task
-    return jsonify({"message": "Task added successfully!", "task": task}), 201
+    with sqlite3.connect(DB_FILE) as conn:
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO tasks(task) VALUES (?)", (task_content,))
+        task_id = cursor.lastrowid
+        conn.commit()
+    
+    return jsonify({"message": "Task added successfully!", "task": {"id": task_id, "task":task_content}}), 201
 
 @app.route("/tasks", methods = ['GET'])
 def get_tasks():
+    with sqlite3.connect(DB_FILE) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, task FROM tasks")
+        rows = cursor.fetchall()
+
+    tasks = [{"id":row[0], "task": row[1]} for row in rows]
     return jsonify({"tasks": todos}), 200
 
 
 @app.route("/tasks/<int:task_id>", methods=['GET'])
 def get_task_by_id(task_id):
-    for task in todos:
-        if task['id'] == task_id:
-            return jsonify({"task": task}), 200
-    return jsonify({"error": "Task not found"}), 404
+    with sqlite3.connect(DB_FILE) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, task FROM tasks WHERE id = ?", (task_id,))
+        row = cursor.fetchone()
+    if row:
+        return jsonify({"task": {"id": row[0], "task": row[1]}}), 200
+    return jsonify({"message": "Task not found!"}), 404
 
 
 @app.route("/tasks/<int:task_id>", methods=['DELETE'])
 def delete_task(task_id):
-    global todos
-
-    for task in todos:
-        if task['id'] == task_id:
-            todos.remove(task)
-            return jsonify({"message": "Task deleted successfully!"}), 200
-
-    # If the task is not found, return an error message
-    return jsonify({"message": "Task was not found!"}), 404
+    with sqlite3.connect(DB_FILE) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, task FROM tasks WHERE id = ?", (task_id,))
+        affected = cursor.rowcount
+        conn.commit()
+        if affected:
+            return jsonify({"message": f"Task {task_id} deleted."}), 200
+        else:
+            return jsonify({"message": "Task not found!"}), 404
+    
 
 
 if __name__ == '__main__':
