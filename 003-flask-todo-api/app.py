5acec85d07de460008af0e79f01c875a0e179f73
shuwang425 <60482034+shuwang425@users.noreply.github.com>
2025-03-26 14:48:11 -0400

    Update

--- 003-flask-todo-api/app.py
+++ 003-flask-todo-api/app.py
@@ -1,8 +1,18 @@
+def get_todos(filepath = "todo.txt"):
+    """ 
+    Read a text file and return a list of to-do items. 
+    """
+    with open(filepath, "r") as file:
+        todos_local = file.readlines()
+    return todos_local
 
-# import sqlite3
-import os
-
-user_prompt = "Enter a todo: "
+def write_todos(todo_arg, filepath = "todo.txt"):
+    """ 
+    Write the to-do items in to the file.
+    """
+    with open(filepath, "w") as file:
+        file.writelines(todo_arg)
+    
 
 while 1:
     user_action = input("Type add, show, edit, delete or exit:")
@@ -11,19 +21,17 @@ while 1:
     if user_action.startswith('add'):
         todo = user_action[4:]
 
-        with open('todo.txt', "r") as file:
-            todos = file.readlines()
+        todos = get_todos()
 
         # Append the newly added todo
         todos.append(todo + "\n")
 
         # Open the file again to overwrite with the new todos
-        with open('todo.txt', "w") as file:
-            file.writelines(todos)
+        write_todos(todos)
+
 
     elif user_action.startswith('show'):
-        with open('todo.txt', "r") as file:
-            todos = file.readlines()
+        todos = get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip("\n")
@@ -36,15 +44,12 @@ while 1:
             print(number)
             number -= 1
 
-            with open('todo.txt', "r") as file:
-                todos = file.readlines()
-                # print("Here is the todos existing.", todos)
+            todos = get_todos()
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + "\n" # Add '\n' to be consistent with rest W
 
-            with open('todo.txt', "w") as file:
-                file.writelines(todos)
+            write_todos(todos)
 
         except ValueError:
             print("Your command is not valid.")
@@ -54,13 +59,11 @@ while 1:
         try:
             number = int(user_action[7:])
 
-            with open("todo.txt", "r") as file:
-                todos = file.readlines()
+            todos = get_todos()
 
             rm_todo = todos.pop(number-1)
 
-            with open("todo.txt", "w") as file:
-                file.writelines(todos)
+            write_todos(todos)
 
             message = f"Todo: {rm_todo} was removed from the list. "
             print(message)
@@ -73,82 +76,4 @@ while 1:
 
     else:
         print("Hey, command is invalid.")
-
-
-# app = Flask(__name__)
-# DB_FILE = 'todo.db'
-
-# # Check if the database file exists, if not create it
-# def init_db():
-#     with sqlite3.connect(DB_FILE) as conn:
-#         cursor = conn.cursor()
-#         cursor.execute('''
-#             CREATE TABLE IF NOT EXISTS tasks (
-#                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-#                 task TEXT NOT NULL
-#             )
-#         ''')
-#         conn.commit()
-
-
-# @app.route('/')
-
-# def home():
-#     return "Flask Todo API with SQLite is running!."
-
-# @app.route("/add", methods=['POST'])
-# def add_task():
-#     data = request.get_json()
-#     task_content = data.get('task')
-
-#     if not task_content:
-#         return jsonify({"Error: Tasck content missing.\n"}), 400
-
-#     with sqlite3.connect(DB_FILE) as conn:
-#         cursor = conn.cursor()
-#         cursor.execute("INSERT INTO tasks(task) VALUES (?)", (task_content,))
-#         task_id = cursor.lastrowid
-#         conn.commit()
-
-#     return jsonify({"message": "Task added successfully!", "task": {"id": task_id, "task":task_content}}), 201
-
-# @app.route("/tasks", methods = ['GET'])
-# def get_tasks():
-#     with sqlite3.connect(DB_FILE) as conn:
-#         cursor = conn.cursor()
-#         cursor.execute("SELECT id, task FROM tasks")
-#         rows = cursor.fetchall()
-
-#     tasks = [{"id":row[0], "task": row[1]} for row in rows]
-#     return jsonify({"tasks": tasks}), 200
-
-
-# @app.route("/tasks/<int:task_id>", methods=['GET'])
-# def get_task_by_id(task_id):
-#     with sqlite3.connect(DB_FILE) as conn:
-#         cursor = conn.cursor()
-#         cursor.execute("SELECT id, task FROM tasks WHERE id = ?", (task_id,))
-#         row = cursor.fetchone()
-#     if row:
-#         return jsonify({"task": {"id": row[0], "task": row[1]}}), 200
-#     return jsonify({"message": "Task not found!"}), 404
-
-
-# @app.route("/tasks/<int:task_id>", methods=['DELETE'])
-# def delete_task(task_id):
-#     with sqlite3.connect(DB_FILE) as conn:
-#         cursor = conn.cursor()
-#         cursor.execute("SELECT id, task FROM tasks WHERE id = ?", (task_id,))
-#         affected = cursor.rowcount
-#         conn.commit()
-#         if affected:
-#             return jsonify({"message": f"Task {task_id} deleted."}), 200
-#         else:
-#             return jsonify({"message": "Task not found!"}), 404
-
-
-
-# if __name__ == '__main__':
-#     init_db()
-#     app.run(debug=True)
 
