f9864d828d015ed744c2d3f64cb33166a4287101
Shu <shuw425@gmail.com>
2025-03-27 23:51:38 -0400

    add delete and exit button. change theme, add clock

--- 003-flask-todo-api/gui.py
+++ 003-flask-todo-api/gui.py
@@ -1,21 +1,31 @@
 import functions
 import FreeSimpleGUI as sg
+import time
 
+sg.theme('Purple')
+clock = sg.Text('', key='clock')
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo", key = "todo")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
-list_box = sg.Listbox(values = functions.get_todos(), key = 'todos',
-                      enable_events = True, size = [45, 10])
+list_box = sg.Listbox(values=functions.get_todos(), key='todos',
+                      enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
+delete_button = sg.Button("Delete")
+exit_button = sg.Button("Exit")
 
-layout = [[label], [input_box, add_button], [list_box, edit_button]]
+layout = [[clock],
+            [label],
+            [input_box, add_button],
+            [list_box, edit_button, delete_button],
+            [exit_button]]
 
 window = sg.Window("To-Do App",
-                   layout = layout ,
+                   layout=layout,
                    font=('Helvetica', 20))
 
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=10)
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     print(1, event)
     print(2, values)
     print(3, values["todos"])
@@ -28,15 +38,31 @@ while True:
             functions.write_todos(todos)
             window['todos'].update(values=todos)
         case "Edit":
-            todo_to_edit = values['todos'][0]
-            new_todo = values["todo"]
+            try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values["todo"]
 
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
 
-            functions.write_todos(todos)
-            window['todos'].update(values = todos)
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+            except IndexError:
+                sg.popup("Please select a todo first")
+                print("Please select a todo first.")
+        case "Delete":
+            try:
+                todo_to_delete = values['todo']
+                todos = functions.get_todos()
+                todos.remove(todo_to_delete)
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value="")
+            except:
+                sg.popup("Please select a todo first.")
+        case "Exit":
+            break
         case "todos":
             window["todo"].update(value = values['todos'][0])
 
