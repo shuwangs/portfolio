00464ac19d4e511debedf460e631e3f48e77f17a
Shu <shuw425@gmail.com>
2025-05-06 23:29:22 -0400

    readin excel files Create title in PDF for each excel

--- 005-excel-to-pdf-invoice/main.py
+++ 005-excel-to-pdf-invoice/main.py
@@ -1,2 +1,26 @@
+import os
+import glob
 import pandas as pd
-import fpdf from FPDF
+from fpdf import FPDF
+from pathlib import Path
+
+filepaths = glob.glob("invoices/*.xlsx")
+# Ensure the folder exists
+os.makedirs("PDFs", exist_ok=True)
+
+for filepath in filepaths:
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+
+    filename = Path(filepath).stem
+    invoice_nr = filename.split("-")[0]
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_nr}")
+
+
+
+    pdf.output(f"PDFs/{filename}.pdf")
+
