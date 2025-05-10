dc5fa4082880b49a8643bd74671bcc637af103b3
Shu <shuw425@gmail.com>
2025-05-10 01:25:51 -0400

    007- updated read temperature files

--- 007-weather-data-api/main.py
+++ 007-weather-data-api/main.py
@@ -3,9 +3,11 @@ import pandas as pd
 
 app = Flask(__name__)
 
+stations = pd.read_csv("data_small/stations.txt", skiprows=17)
+stations = stations[['STAID', 'STANAME                                 ']]
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data =stations.to_html())
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
@@ -16,6 +18,24 @@ def about(station, date):
     return {"station": station,
             "date": date,
             "temperature": temperature}
-    # return render_template("about.html")
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    df.columns = df.columns.str.strip()
+
+    res = df.to_dict(orient="records")
+
+    return res
+@app.route("/api/v1/yearly/<station>/<year>")
+def year_data(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df.columns = df.columns.str.strip()
+    df["DATE"] = df["DATE"].astype(str)
+    res = df[df["DATE"].str.startswith(str(year))].to_dict()
+
+    return res
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
