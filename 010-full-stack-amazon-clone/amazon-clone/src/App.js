18599b70ea0879ed3018d381ffe81f6daef9ff72
Shu <shuw425@gmail.com>
2025-05-12 01:33:38 -0400

    010-create the header part

---
+++ 010-full-stack-amazon-clone/amazon-clone/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './Header.js';
+
+
+function App() {
+  return (
+    // BEM convention
+    <div className="app">
+      <Header />
+    {/* Home */}
+    
+    </div>
+
+  );
+}
+
+
+export default App;
