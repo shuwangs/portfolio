4c8df1ff36b666f8915569e6cda1c1bf177e3d28
Shu <shuw425@gmail.com>
2025-05-13 01:18:47 -0400

    update product.js

--- 010-full-stack-amazon-clone/amazon-clone/src/Home.js
+++ 010-full-stack-amazon-clone/amazon-clone/src/Home.js
@@ -15,17 +15,59 @@ function Home() {
 
                 {/* Product Rows*/}
                 <div className="home__row">
-                   <Product />
-                   <Product />
+                    <Product 
+                        id = "123245"
+                        title = "cute ugly shoes"
+                        price = {39.9}
+                        rating = {3}
+                        image = "https://m.media-amazon.com/images/I/713AO8fIDuL._AC_SY300_SX300_.jpg" 
+                        />
+
+                    <Product 
+                        id = "123245"
+                        title = "cute ugly shoes"
+                        price = {39.9}
+                        rating = {3}
+                        image = "https://m.media-amazon.com/images/I/713AO8fIDuL._AC_SY300_SX300_.jpg" 
+                        />
+                    
+                {/* Product */}
                 </div>
                 <div className="home__row">
-                    {/* Product */}
-                    {/* Product */}
-                    {/* Product */}
-                </div>
+                    <Product 
+                        id = "123245"
+                        title = "cute ugly shoes"
+                        price = {39.9}
+                        rating = {3}
+                        image = "https://m.media-amazon.com/images/I/713AO8fIDuL._AC_SY300_SX300_.jpg" 
+                        />
+                        
+                    <Product 
+                        id = "123245"
+                        title = "cute ugly shoes"
+                        price = {39.9}
+                        rating = {3}
+                        image = "https://m.media-amazon.com/images/I/713AO8fIDuL._AC_SY300_SX300_.jpg" 
+                        />
+                        
+                    <Product 
+                        id = "123245"
+                        title = "cute ugly shoes"
+                        price = {39.9}
+                        rating = {3}
+                        image = "https://m.media-amazon.com/images/I/713AO8fIDuL._AC_SY300_SX300_.jpg" 
+                        />
+                </div>      
                 <div className="home__row">
-                    {/* Product */}
-                </div>                  
+                    <Product 
+                        id = "123245"
+                        title = "cute ugly shoes"
+                        price = {39.9}
+                        rating = {3}
+                        image = "https://m.media-amazon.com/images/I/713AO8fIDuL._AC_SY300_SX300_.jpg" 
+                        />
+
+                </div>                        
 
             </div>
         </div>
--- 010-full-stack-amazon-clone/amazon-clone/src/Product.js
+++ 010-full-stack-amazon-clone/amazon-clone/src/Product.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import './Product.css'
 
-function Product() {
+function Product({title, price, rating, image}) {
   return (
     <div className='product'>
       {/* products contains : a title, price, start rating, and a photo */}
       <div className='product__info'>
-            <p>The Lean Startup</p>
+            <p>{title}</p>
             <p className='product__price'>
             <small>$</small>
-            <strong>20.07</strong>
+            <strong>{price}</strong>
             </p>
             <div className='product__rating'>
-                <p>⭐</p>
+            {/* rating is a number from 1 to 5 */}
+            {Array(rating).fill().map((_, i) => (
+                <p>⭐</p> ))
+            }
+
 
             </div>
        </div>
-       <img src="https://m.media-amazon.com/images/I/61aoOb2QEmL.__AC_SY300_SX300_QL70_FMwebp_.jpg" alt="" />
+       <img src={image} alt="" />
         <button className='product__button'>Add to Basket</button>
     </div>
   )
