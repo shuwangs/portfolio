6aa8aa736c60bd540fa875230971d6845301a19a
Shu <shuw425@gmail.com>
2025-05-16 01:58:32 -0400

    feat: add subtotal dynamicly, needs debug

--- 010-full-stack-amazon-clone/amazon-clone/src/Subtotal.js
+++ 010-full-stack-amazon-clone/amazon-clone/src/Subtotal.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import './Subtotal.css'
 import { NumericFormat } from 'react-number-format';
-function Subtotal({baskketCount, basketTotal}) {
+import { useStateValue } from './StateProvider';
+
+
+
+function Subtotal() {
+  const[{basket}, dispatch] = useStateValue();
+  const basketCount = basket.length; 
+  const basketTotal = basket.reduce((total, item) => total + item.price, 0).toFixed(2); // Assuming each item has a price property
+
   return (
     <div className='subtotal'>
       <NumericFormat
@@ -9,7 +17,7 @@ function Subtotal({baskketCount, basketTotal}) {
           <>
             <p>
                 {/* Replace with the actual number of items in the basket */}
-              Subtotal ({baskketCount} items): <strong>{value}</strong>
+              Subtotal ({basketCount} items): <strong>{value}</strong>
             </p>
             <small className='subtotal__gift'>
               <input type="checkbox" /> This order contains a gift
