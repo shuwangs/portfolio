a0bcba948448cd86606c6e49e17edebbb2a08e0d
Shu <shuw425@gmail.com>
2025-05-23 21:10:47 -0400

    update loginIn page

--- 010-full-stack-amazon-clone/amazon-clone/src/Login.js
+++ 010-full-stack-amazon-clone/amazon-clone/src/Login.js
@@ -1,16 +1,49 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Login.css'
 import { Link } from 'react-router-dom';
 
 function Login() {
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState(""); 
+    const signIn = e=>{
+        e.preventDefault(); // Prevents the page from refreshing
+        // Do some fancy firebase login stuff
+        console.log("Sign In");
+    }
+    const register = e=>{
+        e.preventDefault(); // Prevents the page from refreshing
+        // Do some fancy firebase register stuff
+        console.log("Register");
+    }
+
   return (
     <div className='login'>
         <Link to="/">
         
-      <img className='login__logo'
-        src = "https://pngimg.com/uploads/amazon/amazon_PNG6.png" />
+            <img className='login__logo'
+            src = "https://pngimg.com/uploads/amazon/amazon_PNG6.png" />
 
         </Link>
+        <div className='login__container'>
+            <h1>Sign-In</h1>
+
+            <form>
+                <h5>Email</h5>
+                <input type="text" value = {email} 
+                onChange = {e => setEmail(e.target.value)}/>
+                <h5>Password</h5>
+                <input type="password" value = {password} 
+                onChange = {e=>setPassword(e.target.value)} />
+                <button type = "submit" onClick={signIn}
+                className='login__signInButton' >Sign In</button>
+            
+            </form>
+
+            <p>By signing-in you agree to the AMAZON FAKE CLONE Conditions 
+                of Use & and Privacy Notice. </p>
+            <button  type="submit" onClick = {register} 
+            className='login__registerButton'>Create your Amazon Account</button>
+        </div>
     </div>
 
   )
