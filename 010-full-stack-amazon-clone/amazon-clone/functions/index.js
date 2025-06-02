5defe6aabbacc5b81dc07596f27c0903aed35318
Shu <shuw425@gmail.com>
2025-06-02 01:49:47 -0400

    update

--- 010-full-stack-amazon-clone/amazon-clone/functions/index.js
+++ 010-full-stack-amazon-clone/amazon-clone/functions/index.js
@@ -7,8 +7,10 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-
+// const {onRequest} = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
+const functions = require("firebase-functions/v2");
 const express = require("express");
+const cors = require("cors");
+const stripe = 
 
